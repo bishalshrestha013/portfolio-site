7d4018d41ffbcd5529a5aee45034cef433461b93
Bishal <mr.shrestha713@gmail.com>
2026-02-07 07:59:51 +0545

    feat: integrate shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Portfolio site</div>;
+  return (
+    <div>
+      Portfolio site
+      <Button variant="default">Click me</Button>
+    </div>
+  );
 }
