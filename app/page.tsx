0192f7d36f56c498f9caf9706a18921233d0b884
Bishal <mr.shrestha713@gmail.com>
2026-02-07 16:16:05 +0545

    refactor: add separate component for intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,11 @@
-import photo from "@/public/assets/photo.jpeg";
-import Image from "next/image";
+import Experience from "./components/experience";
+import Intro from "./components/intro";
 
 export default function Home() {
   return (
     <div className="mt-2 flex justify-center">
-      <div className="flex flex-col items-start md:flex-row-reverse border p-4 bg-white md:rounded-4xl gap-4">
-        <Image
-          src={photo}
-          alt="Profile photo"
-          width={100}
-          height={100}
-          className="rounded-4xl"
-        />
-        <div>
-          <h1 className="font-bold text-3xl md:text-5xl mb-4">
-            Bishal Shrestha
-          </h1>
-          <p className="text-zinc-600">
-            Full Stack Software Engineer building web apps. Find me on twitter
-            and linkedin.
-          </p>
-        </div>
-      </div>
+      <Intro />
+      <Experience />
     </div>
   );
 }
