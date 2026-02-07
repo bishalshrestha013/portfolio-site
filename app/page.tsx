1f63006e735fc3515f14f66d0bec20acc0e4940d
Bishal <mr.shrestha713@gmail.com>
2026-02-07 14:48:51 +0545

    feat: add intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import photo from "@/public/assets/photo.jpeg";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="mt-2 flex justify-center">
+      <div className="flex flex-col items-start md:flex-row-reverse border p-4 bg-white md:rounded-4xl gap-4">
+        <Image
+          src={photo}
+          alt="Profile photo"
+          width={100}
+          height={100}
+          className="rounded-4xl"
+        />
+        <div>
+          <h1 className="font-bold text-3xl md:text-5xl">Bishal Shrestha</h1>
+          <p className="text-zinc-600">
+            Full Stack Software Engineer building web apps. Find me on twitter
+            and linkedin.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
